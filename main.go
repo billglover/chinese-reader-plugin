6c0e83baed5184a6679e46630d48fa4b549928f7
Bill Glover <bill@billglover.co.uk>
2017-12-07 08:21:14 +0000

    Switch to multi-character matching algorithm

--- main.go
+++ main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
-	"bufio"
 	"encoding/json"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"unicode"
+
+	"github.com/billglover/chinese-reader/scanner"
 )
 
 type Request struct {
@@ -37,9 +36,8 @@ func handleRequest(rw http.ResponseWriter, req *http.Request) {
 		http.Error(rw, err.Error(), http.StatusInternalServerError)
 	}
 
-	//fmt.Println(mreq)
-
-	score, markup := scoreText(mreq.Text)
+	known := GetKnown()
+	score, markup, _ := scanner.Scan(mreq.Text, known)
 
 	mresp := Response{
 		Text:   mreq.Text,
@@ -53,62 +51,21 @@ func handleRequest(rw http.ResponseWriter, req *http.Request) {
 
 }
 
-func scoreText(t string) (int, string) {
-
-	markup := ""
-	score := 0
-	count := 0
-
+func GetKnown() string {
 	f, err := os.Open("data/words.txt")
 	if err != nil {
 		fmt.Println(err)
-		os.Exit(1)
+		return ""
 	}
 	defer f.Close()
 
-	words := map[string]bool{}
-
-	r := bufio.NewReader(f)
-	for {
-		if b, _, err := r.ReadLine(); err != nil {
-			if err == io.EOF {
-				break
-			} else {
-				fmt.Println(err)
-				return score, markup
-			}
-		} else {
-			words[string(b)] = true
-		}
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		fmt.Println(err)
+		return ""
 	}
 
-	for _, c := range t {
-		if unicode.Is(unicode.Han, c) == true {
-
-			count++
-
-			if ok := words[string(c)]; ok == true {
-				score++
-				markup = markup + "<span class=\"border border-primary text-primary\">" + string(c) + "</span>"
-				continue
-			}
-
-		}
-		markup = markup + string(c)
-	}
-
-	if len(t) == 0 {
-		return 0, markup
-	}
-
-	fmt.Println("score:", score)
-	fmt.Println("count:", count)
-
-	if count == 0 {
-		return 0, markup
-	}
-	score = score * 100 / count
-	return score, markup
+	return string(b)
 }
 
 func main() {
