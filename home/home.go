cccbde19eaec02b11e35a5bb03da99930c67e3f3
Bill Glover <bill@billglover.co.uk>
2018-01-13 22:35:03 +0000

    Lookup words list associated with a given token

--- home/home.go
+++ home/home.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"net/http"
 
+	"github.com/billglover/chinese-reader/scanner"
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/log"
 	"google.golang.org/appengine/urlfetch"
@@ -17,6 +18,12 @@ type Request struct {
 	Token string `json:"token"`
 }
 
+type Response struct {
+	Text   string `json:"string"`
+	Score  int    `json:"readability"`
+	Markup string `json:"markup"`
+}
+
 func handleRequest(rw http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
 
@@ -24,10 +31,6 @@ func handleRequest(rw http.ResponseWriter, r *http.Request) {
 
 	// TODO:
 	// - validate user token
-	// - retrieve user's word list
-	// - scan the file
-	// - return the results
-
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		http.Error(rw, err.Error(), http.StatusInternalServerError)
@@ -48,6 +51,29 @@ func handleRequest(rw http.ResponseWriter, r *http.Request) {
 	if valid == false {
 		http.Error(rw, "invalid token", http.StatusUnauthorized)
 	}
+
+	// TODO:
+	// - retrieve user's word list
+	words, err := retrieveWordsList(ctx, mreq.Token)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+	}
+
+	// TODO:
+	// - scan the file
+	score, markup, _ := scanner.Scan(mreq.Text, words)
+
+	mresp := Response{
+		Text:   mreq.Text,
+		Score:  score,
+		Markup: markup,
+	}
+
+	header := rw.Header()
+	header.Add("Content-Type", "application/json")
+	json.NewEncoder(rw).Encode(mresp)
+
+	// - return the results
 }
 
 func init() {
@@ -67,7 +93,7 @@ func validateToken(ctx context.Context, token string) (bool, error) {
 		scheme = "http"
 	}
 
-	req, _ := http.NewRequest("GET", scheme+"://"+tokenURL+"/token/"+token, nil)
+	req, _ := http.NewRequest("PATCH", scheme+"://"+tokenURL+"/token/"+token+"?action=use", nil)
 
 	client := urlfetch.Client(ctx)
 	resp, err := client.Do(req)
@@ -76,16 +102,46 @@ func validateToken(ctx context.Context, token string) (bool, error) {
 	}
 	defer resp.Body.Close()
 
-	// body, err := ioutil.ReadAll(resp.Body)
-	// if err != nil {
-	// 	return false, fmt.Errorf("unable to read response from internal service")
-	// }
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return false, fmt.Errorf("unable to read response from internal service")
+	}
 
-	// fmt.Println(string(body))
+	log.Infof(ctx, string(body))
 
 	if resp.StatusCode != http.StatusOK {
 		return false, nil
 	}
 
 	return true, nil
+}
+
+func retrieveWordsList(ctx context.Context, token string) (string, error) {
+
+	svcName := "words"
+	wordsURL, err := appengine.ModuleHostname(ctx, svcName, "", "")
+	if err != nil {
+		return "", fmt.Errorf("unable to find service %s", svcName)
+	}
+
+	scheme := "https"
+	if appengine.IsDevAppServer() {
+		scheme = "http"
+	}
+
+	req, _ := http.NewRequest("GET", scheme+"://"+wordsURL+"/words/"+token, nil)
+
+	client := urlfetch.Client(ctx)
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("unable to query internal service")
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("unable to read response from internal service")
+	}
+
+	return string(body), nil
 }
